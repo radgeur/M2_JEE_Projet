08261afb1f14c78d2e21204c1b4a9ea7759d131f
nathanael <nathanael>
2016-12-08 12:58:57 +0100

    Affichage des messages avec l'auteur du message, son texte ainsi que ses hashtags, création d'une classe MessageAffiche pour gérer l'affichage avec Mustache

---
+++ src/main/java/rezozio/MessageAffiche.java
@@ -0,0 +1,20 @@
+package rezozio;
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class MessageAffiche{
+
+    private String user;
+    private String texteMessage;
+    private ArrayList<Hashtag> listHT;
+
+    public MessageAffiche(String user, String texteMessage, ArrayList<Hashtag> lht) {
+        this.user = user;
+        this.texteMessage = texteMessage;
+        this.listHT = lht;
+    }
+
+}
